7266a2620c6d532ae29bb748276a168b46f7128c
sergiokorolev <betobidcom@gmail.com>
2017-09-14 16:53:11 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
-  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :username, length: { maximum: 40 }
   validates :username, format: { with: /\A[[:alnum:]_]+\z/ }
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
