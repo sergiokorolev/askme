e890b477de8c6e5094e90741c940689b63a83ee0
sergiokorolev <betobidcom@gmail.com>
2017-09-14 17:40:44 +0300

    Add user to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
 
-  validates :text, presence: true
+  belongs_to :user
+
+  validates :text, :user, presence: true
   validates :text, length: { maximum: 255 }
 end
