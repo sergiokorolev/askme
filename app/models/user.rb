1250930065ca56e5de6edc66cc9427f5c20f3d5d
sergiokorolev <betobidcom@gmail.com>
2017-09-24 23:16:50 +0300

    Add username_downcase method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   validates :username, length: { maximum: 40 }
   validates :username, format: { with: /\A[a-zA-Z0-9\_]+\Z/ }
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  before_validation :username_downcase
+
+  def username_downcase
+    username.downcase!
+  end
 end
