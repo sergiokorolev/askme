6b7011b3039199677787f9899467ffca9bef4a01
sergiokorolev <betobidcom@gmail.com>
2017-10-01 20:15:22 +0300

    Views refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'http://www.timepassfun.com/wp-content/uploads/2011/12/avatar-celebrities-13.jpg'
+    end
+  end
 end
